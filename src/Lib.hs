3f0a9dc9a02599ed5236da826648aba509c960ab
mottato <micaelaottato@gmail.com>
2020-06-09 19:28:44 -0300

    Punto 4 terminado

--- src/Lib.hs
+++ src/Lib.hs
@@ -75,22 +75,66 @@ modificarPrecisionTiro unaFuncion unTiro = UnTiro {precision = unaFuncion.precis
 modificarAlturaTiro :: (Int->Int)->Tiro->Tiro
 modificarAlturaTiro unaFuncion unTiro =  UnTiro {altura = unaFuncion.altura $unTiro}
 
-tiroQuedaEnCero :: Tiro->Tiro
+tiroQuedaEnCero :: Efecto
 tiroQuedaEnCero = modificarVelocidadTiro (\velocidad->0).modificarPrecisionTiro (\presicion->0).modificarAlturaTiro (\altura->0)
 
-type Obstaculo = Tiro->Tiro
-tunelConRampa :: Obstaculo
-tunelConRampa unTiro
- | (>90).precision $unTiro = modificarVelocidadTiro (*2).modificarPrecisionTiro (\presicion->100).modificarAlturaTiro (\altura->0) $unTiro
- | otherwise               =tiroQuedaEnCero unTiro
+superaTunel :: PuedeSuperar
+superaTunel  =(>90).precision
 
-laguna :: Int->Obstaculo
-laguna largoLaguna unTiro
- | ((>80).velocidad $unTiro) && (between (altura unTiro) 1 5) = modificarAlturaTiro (\altura->div altura largoLaguna) unTiro
- | otherwise                                                  = tiroQuedaEnCero unTiro
+superaLaguna :: PuedeSuperar
+superaLaguna unTiro = ((>80).velocidad $unTiro) && (between (altura unTiro) 1 5)
+
+superaHoyo :: PuedeSuperar
+superaHoyo unTiro= (between (velocidad unTiro) 5 20) && ((>95).precision $unTiro)
 
 
-hoyo :: Obstaculo
-hoyo unTiro 
- |(between (velocidad unTiro) 5 20) && ((>95).precision $unTiro) = tiroQuedaEnCero unTiro
- |otherwise                                                      = tiroQuedaEnCero unTiro
+
+efectoTunelConRampa :: Efecto
+efectoTunelConRampa unTiro
+ | superaTunel unTiro = modificarVelocidadTiro (*2).modificarPrecisionTiro (\presicion->100).modificarAlturaTiro (\altura->0) $unTiro
+ | otherwise          =tiroQuedaEnCero unTiro
+
+efectoLaguna :: Int->Efecto
+efectoLaguna largoLaguna unTiro
+ | superaLaguna unTiro = modificarAlturaTiro (\altura->div altura largoLaguna) unTiro
+ | otherwise           = tiroQuedaEnCero unTiro
+
+
+efectoHoyo :: Efecto
+efectoHoyo unTiro 
+ |superaHoyo unTiro = tiroQuedaEnCero unTiro
+ |otherwise         = tiroQuedaEnCero unTiro
+
+--4)
+
+type Obstaculo = (PuedeSuperar,Efecto)
+type PuedeSuperar = Tiro->Bool
+type Efecto = Tiro->Tiro
+
+tunelConRampa = (superaTunel,efectoTunelConRampa)
+laguna = (superaLaguna,efectoLaguna)
+hoyo = (superaHoyo,efectoHoyo)
+
+puedeSuperar :: Obstaculo->PuedeSuperar
+puedeSuperar =fst
+
+efecto :: Obstaculo->Efecto
+efecto = snd
+
+superaObstaculo :: Obstaculo->Jugador->Palo->Bool
+superaObstaculo obstaculo unJugador unPalo = (puedeSuperar obstaculo) (golpe unPalo unJugador)
+
+palosUtiles :: Jugador->Obstaculo->[Palo]
+palosUtiles unJugador unObstaculo = filter (superaObstaculo unObstaculo unJugador) palos
+
+seSuperan :: [Obstaculo]->Tiro->[Obstaculo]
+seSuperan obstaculos unTiro = takeWhile (\obstaculo->(puedeSuperar obstaculo) $unTiro ) obstaculos
+
+cantidadQueSupera :: [Obstaculo]->Tiro->Int
+cantidadQueSupera obstaculos =length.(seSuperan obstaculos)
+
+paloMasUtil :: Jugador->[Obstaculo]->Palo
+paloMasUtil unJugador obstaculos = maximoSegun (cantidadQueSupera obstaculos.(flip golpe unJugador)) palos
+
+
+
